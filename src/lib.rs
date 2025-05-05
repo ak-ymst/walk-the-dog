61863e04d2cf593a0437f1dc91763163612bd5ad
a.yamashita <ak.ymst@gmail.com>
2025-05-05 15:03:12 +0900

    format

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
+use rand::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
-use rand::prelude::*;
 use web_sys::console;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
@@ -11,7 +11,6 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -19,26 +18,37 @@ pub fn main_js() -> Result<(), JsValue> {
 
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
-    let canvas = document.get_element_by_id("canvas")
-                         .unwrap()
-			 .dyn_into::<web_sys::HtmlCanvasElement>()
-			 .unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
 
     let context = canvas
-                  .get_context("2d")
-		  .unwrap()
-		  .unwrap()
-		  .dyn_into::<web_sys::CanvasRenderingContext2d>()
-		  .unwrap();
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
 
     console::log_1(&JsValue::from_str("Hello World"));
 
-    sierpinki(&context, [(300.0, 0.0),(0.0, 600.0),(600.0, 600.0)],(0, 255, 0), 5);
+    sierpinki(
+        &context,
+        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
+        (0, 255, 0),
+        5,
+    );
 
     Ok(())
 }
 
-fn sierpinki(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3], color: (u8,u8,u8), depth: u8) {
+fn sierpinki(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+    depth: u8,
+) {
     draw_triangle(&context, points, color);
 
     let depth = depth - 1;
@@ -48,23 +58,27 @@ fn sierpinki(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3
         let next_color = get_color(depth);
 
         let left_mid = midpoint(top, left);
-	let right_mid = midpoint(top, right);
-	let bottom_mid = midpoint(left, right);
+        let right_mid = midpoint(top, right);
+        let bottom_mid = midpoint(left, right);
         sierpinki(&context, [top, left_mid, right_mid], next_color, depth);
         sierpinki(&context, [left_mid, left, bottom_mid], next_color, depth);
         sierpinki(&context, [right_mid, bottom_mid, right], next_color, depth);
     }
 }
 
-fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3], color: (u8,u8,u8)) {
+fn draw_triangle(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+) {
     let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
     context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
-    
+
     let [top, left, right] = points;
     context.move_to(top.0, top.1);
     context.begin_path();
     context.line_to(left.0, left.1);
-    context.line_to(right.0 ,right.1);
+    context.line_to(right.0, right.1);
     context.line_to(top.0, top.1);
     context.close_path();
     context.stroke();
@@ -72,17 +86,20 @@ fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64
 }
 
 fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
-    (((point_1.0+point_2.0)/2.0), ((point_1.1+point_2.1)/2.0))
+    (
+        ((point_1.0 + point_2.0) / 2.0),
+        ((point_1.1 + point_2.1) / 2.0),
+    )
 }
 
 fn get_color(depth: u8) -> (u8, u8, u8) {
     let mut rng = thread_rng();
     let range = rand::distributions::Uniform::new(0, 255);
-    
+
     let color = (
-       range.sample(&mut rng),
-       range.sample(&mut rng),
-       range.sample(&mut rng),
+        range.sample(&mut rng),
+        range.sample(&mut rng),
+        range.sample(&mut rng),
     );
 
     return color;
