b422097843d80901491c5ac960d660a9dedced24
a.yamashita <ak.ymst@gmail.com>
2025-05-06 13:20:09 +0900

    remove triangle

--- src/lib.rs
+++ src/lib.rs
@@ -83,13 +83,6 @@ pub fn main_js() -> Result<(), JsValue> {
 
         context.draw_image_with_html_image_element(&image, 0.0, 0.0);
 
-        sierpinki(
-            &context,
-            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-            (0, 255, 0),
-            5,
-        );
-
         let json = fetch_json("rhb.json")
             .await
             .expect("Could not fetch rhb.json");
@@ -145,66 +138,4 @@ async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
     let resp: web_sys::Response = resp_value.dyn_into()?;
 
     wasm_bindgen_futures::JsFuture::from(resp.json()?).await
-}
-
-fn sierpinki(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-    depth: u8,
-) {
-    draw_triangle(&context, points, color);
-
-    let depth = depth - 1;
-
-    let [top, left, right] = points;
-    if depth > 0 {
-        let next_color = get_color();
-
-        let left_mid = midpoint(top, left);
-        let right_mid = midpoint(top, right);
-        let bottom_mid = midpoint(left, right);
-        sierpinki(&context, [top, left_mid, right_mid], next_color, depth);
-        sierpinki(&context, [left_mid, left, bottom_mid], next_color, depth);
-        sierpinki(&context, [right_mid, bottom_mid, right], next_color, depth);
-    }
-}
-
-fn draw_triangle(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-) {
-    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
-    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
-
-    let [top, left, right] = points;
-    context.move_to(top.0, top.1);
-    context.begin_path();
-    context.line_to(left.0, left.1);
-    context.line_to(right.0, right.1);
-    context.line_to(top.0, top.1);
-    context.close_path();
-    context.stroke();
-    context.fill();
-}
-
-fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
-    (
-        ((point_1.0 + point_2.0) / 2.0),
-        ((point_1.1 + point_2.1) / 2.0),
-    )
-}
-
-fn get_color() -> (u8, u8, u8) {
-    let mut rng = thread_rng();
-    let range = rand::distributions::Uniform::new(0, 255);
-
-    let color = (
-        range.sample(&mut rng),
-        range.sample(&mut rng),
-        range.sample(&mut rng),
-    );
-
-    return color;
 }
