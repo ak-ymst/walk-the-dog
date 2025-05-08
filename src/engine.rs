07bf310f6bebc2aba5e92c92d984a650b6f68dc7
a.yamashita <ak.ymst@gmail.com>
2025-05-08 22:35:28 +0900

    add game initialize func

--- src/engine.rs
+++ src/engine.rs
@@ -1,5 +1,6 @@
 use crate::browser;
 use anyhow::{Result, anyhow};
+use async_trait::async_trait;
 use futures::channel::oneshot::channel;
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -38,7 +39,9 @@ pub async fn load_image(source: &str) -> Result<HtmlImageElement> {
     Ok(image)
 }
 
+#[async_trait(?Send)]
 pub trait Game {
+    async fn initialize(&self) -> Result<Box<dyn Game>>;
     fn update(&mut self);
     fn draw(&self, context: &CanvasRenderingContext2d);
 }
@@ -55,6 +58,7 @@ type LoopClosure = Closure<dyn FnMut(f64)>;
 
 impl GameLoop {
     pub async fn start(mut game: impl Game + 'static) -> Result<()> {
+        let mut game = game.initialize().await?;
         let mut game_loop = GameLoop {
             last_frame: browser::now()?,
             accumulated_delta: 0.0,
