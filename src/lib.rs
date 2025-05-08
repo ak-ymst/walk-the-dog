8892911bdf6264f4b2e996014dca7521417898c3
a.yamashita <ak.ymst@gmail.com>
2025-05-08 23:24:30 +0900

    fix main

--- src/lib.rs
+++ src/lib.rs
@@ -43,49 +43,14 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
-
-    let context = browser::context().expect("Could not get browser context");
-
     log!("Hello World 2");
 
     browser::spawn_local(async move {
-        let sheet: Sheet = browser::fetch_json("rhb.json")
+        let game = game::WalkTheDog::new();
+
+        engine::GameLoop::start(game)
             .await
-            .expect("Could not fetch rhb.json")
-            .into_serde()
-            .expect("Could not convert rhb.json into a Sheet structure");
-
-        let image = engine::load_image("rhb.png")
-            .await
-            .expect("Could not load rhb.png");
-
-        let mut frame = -1;
-        let interval_callback = Closure::wrap(Box::new(move || {
-            frame = (frame + 1) % 8;
-            let frame_name = format!("Run ({}).png", frame + 1);
-            context.clear_rect(0.0, 0.0, 600.0, 600.0);
-
-            let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
-            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                &image,
-                sprite.frame.x.into(),
-                sprite.frame.y.into(),
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-                300.0,
-                300.0,
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-            );
-        }) as Box<dyn FnMut()>);
-
-        browser::window()
-            .unwrap()
-            .set_interval_with_callback_and_timeout_and_arguments_0(
-                interval_callback.as_ref().unchecked_ref(),
-                50,
-            );
-        interval_callback.forget();
+            .expect("Could not start game loop");
     });
 
     Ok(())
