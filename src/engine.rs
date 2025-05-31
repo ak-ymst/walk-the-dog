3d715cf23b5c4b6ee5ead186857a914121826f82
a.yamashita <ak.ymst@gmail.com>
2025-05-31 11:05:03 +0900

    remove position property from Image

--- src/engine.rs
+++ src/engine.rs
@@ -36,7 +36,6 @@ pub struct Cell {
 
 pub struct Image {
     element: HtmlImageElement,
-    position: Point,
     bounding_box: Rect,
 }
 
@@ -51,13 +50,12 @@ impl Image {
 
         Self {
             element,
-            position,
             bounding_box,
         }
     }
 
     pub fn draw(&self, renderer: &Renderer) {
-        renderer.draw_entire_image(&self.element, &self.position);
+        renderer.draw_entire_image(&self.element, &self.bounding_box.position);
         renderer.draw_rect(&self.bounding_box());
     }
 
@@ -67,12 +65,10 @@ impl Image {
 
     pub fn move_horizontally(&mut self, distance: i16) {
         self.bounding_box.position.x += distance;
-        self.position.x += distance;
     }
 
     pub fn set_x(&mut self, x: i16) {
         self.bounding_box.set_x(x);
-        self.position.x = x;
     }
 
     pub fn right(&self) -> i16 {
