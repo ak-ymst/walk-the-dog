96f23cf3079f45cec3724ae115d70372ad695734
a.yamashita <ak.ymst@gmail.com>
2025-06-22 13:02:13 +0900

    play bgm

--- src/sound.rs
+++ src/sound.rs
@@ -5,6 +5,19 @@ use wasm_bindgen::JsValue;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{AudioBuffer, AudioBufferSourceNode, AudioContext, AudioDestinationNode, AudioNode};
 
+pub enum LOOPING {
+    NO,
+    YES,
+}
+
+fn create_track_source(ctx: &AudioContext, buffer: &AudioBuffer) -> Result<AudioBufferSourceNode> {
+    let track_source = create_buffer_source(ctx)?;
+    track_source.set_buffer(Some(&buffer));
+    connect_with_audio_node(&track_source, &ctx.destination())?;
+
+    Ok(track_source)
+}
+
 pub fn create_audio_context() -> Result<AudioContext> {
     AudioContext::new().map_err(|err| anyhow!("Could not create audio context: {:#?}", err))
 }
@@ -23,10 +36,11 @@ fn connect_with_audio_node(
         .map_err(|err| anyhow!("Error connecting audio source to destination {:#?}", err))
 }
 
-pub fn play_sound(ctx: &AudioContext, buffer: &AudioBuffer) -> Result<()> {
-    let track_source = create_buffer_source(ctx)?;
-    track_source.set_buffer(Some(&buffer));
-    connect_with_audio_node(&track_source, &ctx.destination())?;
+pub fn play_sound(ctx: &AudioContext, buffer: &AudioBuffer, looping: LOOPING) -> Result<()> {
+    let track_source = create_track_source(ctx, buffer)?;
+    if matches!(looping, LOOPING::YES) {
+        track_source.set_loop(true);
+    }
 
     track_source
         .start()
